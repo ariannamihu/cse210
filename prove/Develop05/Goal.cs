7a5b8d7323780c2a88dc2da20f6d73d76a8af568
Arianna Mihu <mih21002@byui.edu>
2023-11-20 15:57:03 -0700

    2nd commit

--- prove/Develop05/Goal.cs
+++ prove/Develop05/Goal.cs
@@ -6,9 +6,6 @@ public class Goal
     string _description = "";
     int _points = 0;
     
-    List<string> goals = new List<string> {
-
-    };
 
     public string Name
     {
@@ -28,18 +25,48 @@ public class Goal
 
     // }
 
-    // public int RecordEvent()
-    // {
-
-    // }
+    public int RecordEvent()
+    {
+        return _points;
+    }
 
     // public string ToSavedString()
     // {
 
     // }
 
+    // public void SaveToFile(string file)
+    // {
+    //     using (StreamWriter writer = new StreamWriter(file, true)) 
+    //     {
+    //         foreach (var entry in JournalEntriesList)
+    //         {
+    //             writer.WriteLine($" {entry._date}, {entry._prompt}, {entry._response}");
+    //         }
+    //     }
+    //     Console.WriteLine("The journal entry has been saved.");
+    // }
+
+    // public void LoadFromFile(string file)
+    // {
+    //     using (StreamReader reader = new StreamReader(file, true))
+    //     {
+    //         var line = reader.ReadLine();
+    //         while(line!=null)
+    //         {
+    //             var splitLine = line.Split(",");
+    //             var newEntry = new JournalEntry(splitLine[0], splitLine[1], splitLine[2]);
+    //             JournalEntriesList.Add(newEntry);
+    //             Console.WriteLine(line);
+    //             line = reader.ReadLine();
+    //         }
+    //     }
+    // }
+
     public override string ToString()
     {
-        return $"{_name} ({_description}) and {_points} points";
+        return $"{_name}|{_description}|{_points}";
     }
+
+
 }
--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,13 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
+        int _totalPoints = 0;
+        int _goalOption = 0;
+        string _goalName = "";
+        string _goalDescription  = "";
+        int _goalPoints = 0;
+
+        List<string> goalsList = new List<string> {
+
+        };
+        
         bool quit = false;
         
         while (!quit)
         {
             Console.WriteLine("Menu:");
-            Console.WriteLine("1. Create New Goal");
-            Console.WriteLine("2. List Goals");
+            Console.WriteLine("1. Display Score");
+            Console.WriteLine("2. Create New Goal");
             Console.WriteLine("3. Save Goals");
             Console.WriteLine("4. Load Goals");
             Console.WriteLine("5. Record Event");
@@ -22,31 +32,38 @@ class Program
                 switch (choice)
                 {
                     case 1:
-                        Console.WriteLine("The types of Goals are: \n1. Simple Goal \n2. Eternal Goal \n3. Checklist Goal \nWhich type of goal would you like to create? ");
-                        string goalOption = Console.ReadLine();
-                        Console.WriteLine("What is the name of your goal?");
-                        string goalName = Console.ReadLine();
-                        Console.WriteLine("What is its description? ");
-                        string goalDescription = Console.ReadLine();
-                        Console.WriteLine("Points? ");
-                        int goalPoints = int.Parse(Console.ReadLine());
+                        Console.WriteLine($"Your score is {_totalPoints}");
 
-                        Goal goal1 = new Goal(goalName, goalDescription, goalPoints);
+                        break;
+                    case 2:
+                        Console.WriteLine("The types of Goals are: \n1. Simple Goal \n2. Eternal Goal \n3. Checklist Goal \nWhich type of goal would you like to create? ");
+                        _goalOption = int.Parse(Console.ReadLine());
+                        
+                        Console.WriteLine("What is the name of your goal?");
+                        _goalName = Console.ReadLine();
+
+                        Console.WriteLine("What is its description? ");
+                        _goalDescription = Console.ReadLine();
+
+                        Console.WriteLine("Points? ");
+                        _goalPoints = int.Parse(Console.ReadLine());
+
+                        Goal goal1 = new Goal(_goalName, _goalDescription, _goalPoints);
 
                         Console.WriteLine(goal1);
 
                         break;
-                    case 2:
-
-                        break;
                     case 3:
+                        Goal goal2 = new Goal(_goalName, _goalDescription, _goalPoints);
 
+                        goalsList.Add(goal2.ToString());
                         break;
                     case 4:
+                        goalsList.ForEach(p => Console.WriteLine(p));
 
                         break;
                     case 5:
-
+                        
                         break;
                     case 6:
                         quit = true;
